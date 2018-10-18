f27751bf16528f58d4bf8baa424ab1697458cfc8
小红帽 <1399208411@qq.com>
2018-10-18 20:24:47 +0800

    管理端 完成列表表格组件的开发

---
+++ manager/src/components/DataTable.vue
@@ -0,0 +1,80 @@
+<!--  -->
+<template>
+  <el-table
+    class="data-table"
+    :data="dataList"
+    border>
+    <el-table-column
+      type="selection"
+      width="55">
+    </el-table-column>
+    <el-table-column prop="date" label="日期"></el-table-column>
+    <el-table-column prop="name" label="姓名"></el-table-column>
+    <el-table-column prop="address" label="地址"></el-table-column>
+    <el-table-column  prop="" fixed="right" label="操作" width="50">
+      <template slot-scope="scope">
+        <el-dropdown placement="">
+        <span class="more-handle">
+          <i class="icon-more-handle el-icon-more"></i>
+        </span>
+          <el-dropdown-menu slot="dropdown">
+            <slot :index="scope.$index" :rowData="scope.row"></slot>
+            <!--<el-dropdown-item @click.native="test(scope.$index, scope.row)">编辑</el-dropdown-item>-->
+            <!--<el-dropdown-item>删除</el-dropdown-item>-->
+          </el-dropdown-menu>
+        </el-dropdown>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+<script>/* eslint-disable indent,semi */
+export default {
+  props: {
+    dataList: {
+      type: Array
+    },
+    handleConfig: {
+      type: Array
+    }
+  },
+  data() {
+    return {};
+  },
+  components: {},
+
+  computed: {},
+
+  created() {
+  },
+
+  mounted() {
+  },
+
+  methods: {
+    test(index, row) {
+      console.log(index);
+      console.log(row);
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .data-table {
+    line-height: 16px;
+  }
+
+  .more-handle {
+    text-align: center;
+  }
+
+  .icon-more-handle {
+    font-size: 20px;
+    text-align: center;
+    color: #969896;
+  }
+
+  .icon-more-handle:hover {
+    color: #00B7FF;
+  }
+</style>
--- manager/src/views/form/index.vue
+++ manager/src/views/form/index.vue
@@ -1,15 +1,54 @@
 <!--  -->
 <template>
   <div>
-    表单流程
+    表单流
+    <data-table :dataList="dataList">
+      <template slot-scope="props">
+        <el-dropdown-item @click.native="test(props.index, props.rowData)">编辑</el-dropdown-item>
+        <el-dropdown-item>删除</el-dropdown-item>
+      </template>
+        <!--<el-dropdown-item>编辑</el-dropdown-item>-->
+        <!--<el-dropdown-item>删除</el-dropdown-item>-->
+    </data-table>
   </div>
 </template>
 <script>/* eslint-disable indent,semi */
+import DataTable from '../../components/DataTable.vue'
+
 export default {
   data() {
-    return {};
+    return {
+      dataList: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }],
+      handleConfig: [
+        {
+          text: '复制'
+        },
+        {
+          text: '编辑'
+        }
+      ]
+    };
   },
-  components: {},
+  components: {
+    DataTable,
+  },
 
   computed: {},
 
@@ -19,10 +58,27 @@ export default {
   mounted() {
   },
 
-  methods: {}
+  methods: {
+    test(index, rowData) {
+      console.log(index);
+      console.log(rowData);
+    }
+  }
 }
 </script>
 
 <style scoped>
+  .more-handle {
+    text-align: center;
+  }
 
+  .icon-more-handle {
+    font-size: 20px;
+    text-align: center;
+    color: #969896;
+  }
+
+  .icon-more-handle:hover {
+    color: #00B7FF;
+  }
 </style>
